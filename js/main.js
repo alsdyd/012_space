013bf73b79f7a6d83d38c0615f21adb7f14675e2
alsdyd <alsdyd3544@naver.com>
2018-04-13 00:31:00 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 $(function(){
 
 
+$(window).ready(function(){
+	//로딩중일때,
+
+	$('.star').addClass('blind');
+});
+
 
 	$(window).load(function(){
 		//로딩이 완료되었을때,
 
 		$('.out-line01').css('transition','1.1s');
-
-
 
 	});
 
@@ -18,8 +22,11 @@ $(function(){
 
 			$('.out-line01').toggleClass('switch-off');
 			$('.switch-btn').toggleClass('switch-color');
-			
+			$('.star').toggleClass('blind');
+
 		});
+
+
 
 
 
